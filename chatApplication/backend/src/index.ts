3e31a207141702bc409cc0cc00b29e6f44cb0c66
ezAakash <oenmazoro@gmail.com>
2026-03-05 17:34:36 +0530

    made some tweaks to understand it better

--- chatApplication/backend/src/index.ts
+++ chatApplication/backend/src/index.ts
@@ -2,40 +2,58 @@ import { WebSocketServer, WebSocket } from 'ws'
 
 const ws = new WebSocketServer({ port: 8080 })
 
-type Users = {
-    socket: WebSocket, 
-    room: String
-}
-
-let allSockets: Users[] = [];
+const allSockets = new Map<string, WebSocket[]>();
 
 // webSocket only takes either string or binary.
+let currentUserRoom: string = "";
 
 ws.on("connection", (socket) => {
     
     socket.on("message", (message) => {
         // string to an object 
-        //@ts-ignore
-        const parsedMessage = JSON.parse(message);
-        if (parsedMessage.type === "join") {
-            allSockets.push({
-                socket,
-                room: parsedMessage.payload.roomId
-            })
-        }
-
-        if (parsedMessage.type === "chat") {
+        try{  
             //@ts-ignore
-            let currentUserRoom = allSockets.find((s) => s.socket === socket).room
-            for (let i = 0 ; i < allSockets.length; i++) {
-                if (currentUserRoom === allSockets[i]?.room) {
-                    allSockets[i]?.socket.send(parsedMessage.payload.message)
+            const parsedMessage = JSON.parse(message);
+            if (parsedMessage.type === "join") {
+    
+                currentUserRoom = parsedMessage.payload.roomId;
+    
+                if (!allSockets.has(currentUserRoom)) {
+                    allSockets.set(currentUserRoom, [])
                 }
+    
+                allSockets.get(currentUserRoom)!.push(socket)
+                socket.send("Room Joined")   
             }
-        }       
-})
+    
+            if (parsedMessage.type === "chat") {
+                let targetRoom = parsedMessage.payload.roomId;
+                let sockets = allSockets.get(targetRoom);
+                
+                if(sockets) {
+                    sockets.forEach((soc) => {
+                        if (soc.readyState === WebSocket.OPEN){
+                            soc.send(parsedMessage.payload.message)
+                        }
+                    })
+                }
+            }       
+
+        }
+        catch(err) {
+            socket.send("You sent a invalid input")
+        }
+    })
+
+    socket.on("close", () => {
+        if (currentUserRoom) {
+            const sockets = allSockets.get(currentUserRoom)
+            if (sockets) {
+                allSockets.set(currentUserRoom, sockets.filter((soc) => soc !== socket))
+            }
+        }
+    })
 
 })
-
 
 
